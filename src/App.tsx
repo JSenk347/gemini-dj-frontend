3beaf126d0e3f2473f6bce819c8ea6a068c9da74
Jordan Senko <oq066776@gmail.com>
2026-01-05 00:18:01 -0700

    tiny changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ function App() {
     <>
       <header className='max-w-4xl mx-auto px-6>'>
         <h1 className="text-6xl text-slate-700 text-center py-10">
-  
           Gemini DJ
         </h1>
       </header>
